a7a64c15346e7d314d3492cb0bb66269191c9b0c
Daniel Sikorski <dadi.sikorski@gmail.com>
2017-08-15 00:00:44 +0200

    curtana: dex2oat: use 8 cores instead of 4
    use more cores for faster app installation
    
    Change-Id: I7045673a28eed043ec45013b1abc48e5f3a5f7ba
    Signed-off-by: Albert I <krascgq@outlook.co.id>

--- properties.mk
+++ properties.mk
@@ -218,4 +218,5 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 
 # Dalvik
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-     dalvik.vm.dex2oat64.enabled=true
+     dalvik.vm.dex2oat64.enabled=true \
+     ro.sys.fw.dex2oat_thread_count=8
