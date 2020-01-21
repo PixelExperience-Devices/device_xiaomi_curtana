268f186e074cf4e449ede43f0d5764a01824d1aa
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2020-01-21 12:25:17 +0530

    sm6250-common: Enable power-libperfmgr
    Change-Id: Ia5f8fb4a2afc7a453eb09d2092a7872fd8442bc9

--- device.mk
+++ device.mk
@@ -393,11 +393,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
-    $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
+    android.hardware.power@1.3-service.xiaomi_sm6250-libperfmgr
 
 # Public Libraries
 PRODUCT_COPY_FILES += \
