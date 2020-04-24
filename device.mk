e240b8497931ae5597f61835b41bec57b68d05ee
Roshan Pius <rpius@google.com>
2020-04-24 14:33:28 -0700

    sm6250-common: Add WifiOverlay
    Bug: 148617260
    Test: Device boots up and connects to wifi networks.
    Change-Id: Iec27a6d947a232a42451ef095dc73d1a52327db5

--- device.mk
+++ device.mk
@@ -540,7 +540,8 @@ PRODUCT_PACKAGES += \
     libwifi-hal-qcom \
     wcnss_service \
     wpa_supplicant \
-    wpa_supplicant.conf
+    wpa_supplicant.conf \
+    WifiOverlay
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
