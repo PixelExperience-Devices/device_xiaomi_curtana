3d5d81739db01b8fd042a9b1cd76deb035b18ada
Lucchetto <lucchetto.tie@live.com>
2020-09-13 06:59:51 +0530

    curtana: downrev to composer 2.2

--- device.mk
+++ device.mk
@@ -200,7 +200,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.mapper@2.0-impl-qti-display \
-    android.hardware.graphics.composer@2.3-service \
+    android.hardware.graphics.composer@2.2-service \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
     android.hardware.renderscript@1.0-impl \
