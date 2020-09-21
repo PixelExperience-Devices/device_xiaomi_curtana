f06827fb2d752e75e4905d186062b77d1ffaf853
Volodymyr Zhdanov <wight554@gmail.com>
2020-09-22 02:01:28 +0300

    curtana: Add Contexthub HAL 1.1

--- device.mk
+++ device.mk
@@ -162,6 +162,13 @@ PRODUCT_PACKAGES += \
     com.quicinc.cne \
     services-ext
 
+# Context hub HAL
+PRODUCT_PACKAGES += \
+    android.hardware.contexthub@1.1-service.generic
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.context_hub.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.context_hub.xml
+
 # Configstore
 PRODUCT_PACKAGES += \
     android.hardware.configstore@1.0-service
