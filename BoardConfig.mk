b399cc5340d986692a6a02006a203408cd3bd568
Nick C <ctwoon@pixelexperience.org>
2020-10-24 12:39:13 +0000

    curtana: update vendor security patch

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -211,7 +211,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2020-07-01
+VENDOR_SECURITY_PATCH := 2020-10-01
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
