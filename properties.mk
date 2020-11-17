fa727bdcdfa0777ec656db13ef27cc5793a0c266
kerneltoast <sultan@kerneltoast.com>
2020-11-17 14:36:57 +0000

    curtana: force tripple frame buffers

--- properties.mk
+++ properties.mk
@@ -150,7 +150,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.disable_skip_validate=1 \
     vendor.display.enable_default_color_mode=1 \
     vendor.gralloc.enable_fb_ubwc=1 \
-    debug.gralloc.enable_fb_ubwc=1
+    debug.gralloc.enable_fb_ubwc=1 \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3
 
 # FRP
 PRODUCT_PROPERTY_OVERRIDES += \
